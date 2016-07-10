71ab69e27b2ae6fc513841a0926d1e27838d64bf
Steve Groesz <wolfpackmars2@yahoo.com>
2016-07-10 03:58:08 -0500

    fix watch not working correctly

--- watch.sh
+++ watch.sh
@@ -4,4 +4,4 @@
 /home/sdtd/unwatch.sh $1
 
 # Start keepalive
-(echo "*/1 * * * * 7dtd.sh start $1"; crontab -l) | crontab -
+(echo "*/1 * * * * /usr/local/bin/7dtd.sh start $1"; crontab -l) | crontab -
